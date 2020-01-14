af7e9a68902f01663b18a3dfe833c00e5001d9a7
Jonathan Domingues <domingues.jonathan95@gmail.com>
2020-01-14 21:20:57 +0100

    Check module is typeof of object

--- public/javascripts/breadcrumb/breadcrumb.js
+++ public/javascripts/breadcrumb/breadcrumb.js
@@ -1,30 +1,32 @@
-module.exports = class Breadcrumb {
-    constructor() {
-        this.data = {
-            location: []
-        };
-    };
-
-    /**
-     * 
-     * @param {*} path - Path is required to construct breadcrumb. Use req.path.
-     *
-     */
-    setBreadcrumb(path) {
-        if(typeof path !== 'undefined') {
-            switch(path) {
-                case "/":
-                    this.data.location.push({ name: "Connexion", path: "/" });
-                break;
-
-                case "/dashboard":
-                    this.data.location.push({ name: "Déconnexion", path: "/" });
-                    this.data.location.push({ name: "Dashboard", path: "/dashboard" });
-                break;
+if(typeof module === 'object') {
+    module.exports = class Breadcrumb {
+        constructor() {
+            this.data = {
+                location: []
             };
-
-            return this.data;
         };
-        
+    
+        /**
+         * 
+         * @param {*} path - Path is required to construct breadcrumb. Use req.path.
+         *
+         */
+        setBreadcrumb(path) {
+            if(typeof path !== 'undefined') {
+                switch(path) {
+                    case "/":
+                        this.data.location.push({ name: "Connexion", path: "/" });
+                    break;
+    
+                    case "/dashboard":
+                        this.data.location.push({ name: "Déconnexion", path: "/" });
+                        this.data.location.push({ name: "Dashboard", path: "/dashboard" });
+                    break;
+                };
+    
+                return this.data;
+            };
+            
+        };
     };
-};
+}
