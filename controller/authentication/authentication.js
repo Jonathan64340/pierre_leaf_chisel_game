90cd07e2ae261a9c4b9ae7f90572a7d9e4a573f2
Jonathan Domingues <domingues.jonathan95@gmail.com>
2020-01-19 21:58:24 +0100

    Hide credentials to db env data

--- controller/authentication/authentication.js
+++ controller/authentication/authentication.js
@@ -2,16 +2,18 @@ const MongoClient = require('mongodb').MongoClient;
 
 class Authentication {
     constructor() {
+
+        // First login to db
+        const uri = process.env.DB_CONN;
+        this.client = new MongoClient(uri, { useNewUrlParser: true, userUnifiedTopology: true });
         
     };
 
     doLogin(user='', password='') {
         return new Promise((resolve, reject) => {
             // Connect to DB
-            const uri = "mongodb+srv://domjon64:I3iUTp7j7L8SzuRZ@game-kattu.mongodb.net";
-            const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-            client.connect(err => {
-                const collection = client.db("game").collection("users");
+            this.client.connect(err => {
+                const collection = this.client.db("game").collection("users");
                 // perform actions on the collection object
                 collection.findOne({ 'username': user }, function (err, user) {
                     if(user !== null) {
@@ -22,7 +24,8 @@ class Authentication {
                 });
 
                 if(err) {
-                    client.close();
+                    console.log('error de connexion')
+                    this.client.close();
                     reject(err);
                 };
             });
