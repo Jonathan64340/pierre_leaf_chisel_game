20d356452311510c02f7e50742039eee27c1660b
Jonathan Domingues <domingues.jonathan95@gmail.com>
2020-01-23 00:26:23 +0100

    Try to use cors

--- index.js
+++ index.js
@@ -31,6 +31,21 @@ app.use(express.static('public'));
 app.use(router);
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+
+    // authorized headers for preflight requests
+    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+
+    app.options('*', (req, res) => {
+        // allowed XHR methods  
+        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+        res.send();
+    });
+});
+
 // Listen server
 app.listen(process.env.PORT, function () { 
     console.log('Server is running on port : ' + process.env.PORT);
