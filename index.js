5fc3e4bcd731455555e7780d5d34287781cb71e0
Jonathan Domingues <domingues.jonathan95@gmail.com>
2020-01-23 00:33:52 +0100

    use cors

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ dotenv.config();
 // Initialize app
 const app = express();
 
+// Use cors
+app.use(cors());
+
 //Use bodyParser
 app.use(bodyParser.json());
 
@@ -29,22 +32,6 @@ app.use(express.static('public'));
 
 // Bind routes
 app.use(router);
-app.use(cors());
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-
-    // authorized headers for preflight requests
-    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-
-    app.options('*', (req, res) => {
-        // allowed XHR methods  
-        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
-        res.send();
-    });
-});
 
 // Listen server
 app.listen(process.env.PORT, function () { 
