e88253489385e0673ab039bd9c1cc00370798f84
Jonathan Domingues <domingues.jonathan95@gmail.com>
2020-05-02 16:49:56 +0200

    show avatar & username on dashboard

--- public/javascripts/dashboard/dashboard.js
+++ public/javascripts/dashboard/dashboard.js
@@ -3,6 +3,20 @@ class Dashboard {
         if (typeof localStorage.getItem('token') === "undefined" || localStorage.getItem('token') === null) {
             return window.location = "/";
         }
+        
+        this.state = {
+            avatar: JSON.parse(localStorage.getItem('USER')).avatar,
+            username: JSON.parse(localStorage.getItem('USER')).username
+        }
+
+        let that = this;
+
+        window.onload = function () {
+            let avatar = document.getElementById('avatar').setAttribute('src', that.state.avatar);
+            let username = document.getElementById('username').innerText = " " + that.state.username;
+        }
+
+        
 
     }
     click() {
